06e5ca10767436cb34e26735f4f36a283b524169
jan-mski <36985396+jan-mski@users.noreply.github.com>
2023-07-23 13:59:49 +0200

    fix test config

--- omnitool/tests/omnitool/test_main.py
+++ omnitool/tests/omnitool/test_main.py
@@ -1,9 +1,7 @@
-from unittest.mock import patch
-
 from omnitool.main import main
 
 
-@patch("omnitool.plugin.loader.load_plugins")
-def test_main(mock_load_plugins):
+def test_main(mocker):
+    mock_load_plugins = mocker.patch("omnitool.plugin.loader.load_plugins")
     main([])
     mock_load_plugins.assert_called_once()
