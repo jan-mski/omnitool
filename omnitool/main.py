bde7e9a9b381d9d00520369900f535efeaeba8cd
jan-mski <36985396+jan-mski@users.noreply.github.com>
2025-06-17 19:56:06 +0200

    update main

--- omnitool/main.py
+++ omnitool/main.py
@@ -1,7 +1,7 @@
 import logging
 import sys
 
-from omnitool import settings
+from omnitool import settings, cli
 from omnitool.plugin.loading import loader
 
 
@@ -15,18 +15,14 @@ logging.basicConfig(
     ]
 )
 
-
-def main(args):
-    logger.info("Starting omnitool")
+def initialize_app():
     settings.load_settings()
     loader.load_plugins()
+    cli.initialize_operations()
 
-    if not loader.loaded_plugins:
-        logger.error("No plugins loaded, exiting")
-        return
+# Init app here for CLI completion to work
+initialize_app()
 
-    logger.info("Omnitool started")
-
-
+# Note: this code is not executed when running from CLI, cli.app() is called directly as per pyproject.toml
 if __name__ == "__main__":
-    main(sys.argv)
+    cli.app()
