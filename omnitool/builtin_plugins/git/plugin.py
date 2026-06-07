f9961866efa49bc94c3569edaa0540a8b1b90bc6
jan-mski <36985396+jan-mski@users.noreply.github.com>
2026-06-07 18:06:46 +0200

    git plugin placeholder

---
+++ omnitool/builtin_plugins/git/plugin.py
@@ -0,0 +1,28 @@
+import logging
+
+from omnitool.plugin.api.context import Context, ResourceData
+from omnitool.plugin.api.definition import PluginDefinition
+
+logger = logging.getLogger(__name__)
+
+class GitRepository(ResourceData):
+    pass
+
+
+definition = PluginDefinition(root_operation_name="git", resource_data_type=GitRepository)
+
+
+@definition.operation
+def checkout(branch: str, context: Context) -> None:
+    """
+    Checks out a branch in the git repository.
+    """
+    logger.info(f"Checking out branch: {branch}")
+
+
+@definition.context_loader
+def load_context(context: Context) -> None:
+    """
+    Loads the context for the git plugin.
+    """
+    logger.info("Loading context for git plugin")
